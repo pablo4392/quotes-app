79948be871e327961d7c0a991c9ec3935e992dde
Pablo C Ramirez <carp4392@gmail.com>
2021-05-03 13:51:52 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import React, {useState} from 'react';
 
 function App() {
   const [randomNumber, setRandom ] = useState(0);
-  const [backgroundColor, setBackgroundColor] = useState("#eeebdd")
+  const [backgroundColor, setBackgroundColor] = useState("#eeebdd");
   const random = Math.random() * (Data.quotes.length);
-  const twittURL = `https://twitter.com/intent/tweet?text=${Data.quotes[randomNumber].quote}%20${Data.quotes[randomNumber].author}`
+  const twittURL = `https://twitter.com/intent/tweet?text=${Data.quotes[randomNumber].quote}%20${Data.quotes[randomNumber].author}`;
+  const quote = Data.quotes[randomNumber].quote;
+  const author = Data.quotes[randomNumber].author;
 
   const handleRandomNumber = () => {
       setRandom(Math.floor(random));
   
       const letterGenerator = () => {
-          var letras = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
-          var numero = (Math.random()*15).toFixed(0);
-          return letras[numero];
+          var letters = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+          var number = (Math.random()*15).toFixed(0);
+          return letters[number];
       }
 
       setBackgroundColor(() => {
-          var coolor = "";
+          var color = "";
           for(var i=0;i<6;i++){
-              coolor = coolor + letterGenerator() ;
+              color = color + letterGenerator() ;
           }
-          return "#" + coolor;
+          return "#" + color;
       })
   }
 
   return (
     <div className="App" style={{background: backgroundColor}}>
-      <QuoteBox quoteBoxText={Data.quotes[randomNumber].quote} quoteBoxAuthor={Data.quotes[randomNumber].author} twitterRef={twittURL} handleClick={handleRandomNumber} />
+      <QuoteBox quoteBoxText={quote} quoteBoxAuthor={author} twitterRef={twittURL} handleClick={handleRandomNumber} />
     </div>
   );
 }
